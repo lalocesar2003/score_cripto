32215b2f782896bd5adf1553f8071caf26107d26
cesar eduardo <ceed1000@gmail.com>
2024-01-27 00:55:49 -0500

    second commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
+import { UserModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath:'.env',
+    }),
+    DatabaseModule,UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
