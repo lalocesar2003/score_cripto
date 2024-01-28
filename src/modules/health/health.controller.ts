fffaf0e07c92a1fc157016d07420db7dcbab3981
cesar eduardo <ceed1000@gmail.com>
2024-01-28 11:55:18 -0500

    health added

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import {
+    HealthCheck,
+    HealthCheckResult,
+    HealthCheckService,
+    HealthIndicatorResult,
+    TypeOrmHealthIndicator,
+  } from '@nestjs/terminus';
+  
 
 @Controller('health')
-export class HealthController {}
+export class HealthController {
+    constructor(
+        private readonly health: HealthCheckService,
+        private readonly db: TypeOrmHealthIndicator,
+      ) {}
+    
+
+      @Get()
+      @HealthCheck()
+      readiness(): Promise<HealthCheckResult> {
+        return this.health.check([
+          async (): Promise<HealthIndicatorResult> =>
+            this.db.pingCheck('database', {
+              timeout: 5000,
+            }),
+        ]);
+      }
+}
