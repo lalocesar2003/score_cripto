e1739c434ac14ee4ad9d7bc1539055aed167f1ab
cesar eduardo <ceed1000@gmail.com>
2024-01-29 07:58:12 -0500

    finish database

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -11,9 +11,10 @@ import {ConfigModule,ConfigService} from '@nestjs/config';
             type: 'mysql',
             host: configService.get('DB_HOST'),
             port: configService.get('DB_PORT'),
+          
             username: configService.get('DB_USERNAME'),
             password: configService.get('DB_PASSWORD'),
-            database: configService.get('DB_DATABASE'),
+            database: configService.get('DB_NAME'),
             entities: [User],
             synchronize: true,
             }
