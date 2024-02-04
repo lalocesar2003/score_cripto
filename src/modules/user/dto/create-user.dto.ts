01d9c12bb12b414d729769ef4d92c560bc0efa98
cesar eduardo <ceed1000@gmail.com>
2024-02-04 15:05:47 -0500

    add reports

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -2,6 +2,8 @@ import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 export class createUserDto {
     @IsEmail()
     email: string;
+    @IsNotEmpty()
+    username: string;
     @IsString()
     password: string;
 }
